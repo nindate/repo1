d67ac953e794d71fdb0671a006fe13ba7149efe6
nindate <nindate@gmail.com>
2014-08-13 22:15:36 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<link rel="stylesheet" href="css/style.css" type="text/css">
+<body>
+
+<?php
+$form = "<form action='./index.php' method='post'>
+	<div id='field1'>
+			<table>
+					<tr>
+					<td>Username:</td>
+					<td><input type='text' name='user' /></td>
+					<td><input id='btn' type='submit' name='userbtn' value='Enter'/></td>
+					</tr>
+			
+			</table>
+	</div>";
+echo $form
+?>
+
+
+</body>
