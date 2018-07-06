7d5ae1353f94cb03e6506a6337566e4aa59c3b4e
root <root@contacts1.c.cloudlogin-999999.internal>
2018-07-06 07:07:48 +0000

    new commit

--- site-contacts/listcontacts.php
+++ site-contacts/listcontacts.php
@@ -79,46 +79,23 @@ echo "<span class='error'>*Presently there are no entries in the database</span>
 }
 
 echo "<table border='1'>";
-echo "<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Contact No</th><th>Comments</th><th></th></tr>";
+echo "<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Contact No</th><th>Comments</th></tr>";
 while($row = mysqli_fetch_array($result)) {
 
-  // echo "<li>";
 
   echo "<tr>";
   echo "<td>" . $row['ID'] . "</td><td>" . $row['FirstName'] . "</td><td> " . $row['LastName'] . "</td><td>" . $row['ContactNo'] . "</td><td>" . $row['Comments'] . "</td>";
 
   $thisid=$row['ID'];
 
-  echo "<td><input type='checkbox' name='checkboxvar[]' value='$thisid'>Delete</td>";
   echo "</tr>";
 
-  // echo "</li>";
 
 }
 echo "</table>";
-  echo "</ul>";
-  echo "<input type='submit' name='Delete' value='Delete selected entries'>";
 echo "</form>";
 
 ?>
-
-<hr>
-<br>
-<p> <h3> Add new contact </h3> </p>
-<p><span class="error">* required field.</span></p>
-
-<form method='post' action='updatedb.php'>
-   First Name:  <span class="error">* </span> <input type="text" name="firstname" value="<?php echo $firstname;?>">
-   <?php echo $firstnameErr;?>
-   Last Name: <span class="error">* </span> <input type="text" name="lastname" value="<?php echo $lastname;?>">
-   <?php echo $lastnameErr;?></span>
-   Contact No:  <span class="error">* </span> <input type="text" name="contactno" value="<?php echo $contactno;?>">
-   <?php echo $ageErr;?></span>
-   <br><br>Comments: <textarea name="comment" rows="2" cols="60"><?php echo $comment;?></textarea>
-   <br><br>
-   <input type="submit" name="Update" value="Update">
-</form>
-
 
 
 
--- site-contacts/updatedb.php
+++ site-contacts/updatedb.php
@@ -16,15 +16,10 @@ $new_contactno=$_POST['contactno'] ;
 
 $new_comment=$_POST['comment'] ;
 
-//echo "$new_firstname $new_lastname $new_age $new_comment"
 
 // Create connection
 
 $con=mysqli_connect("localhost","user1","password","test2");
-// $con=mysqli_connect("127.3.104.2:3306","user1","password","php");
-// $con=mysqli_connect("10.0.2.21","user1","openstack","test");
-// $con=mysqli_connect("192.168.1.72","user1","openstack","test");
-
 
 
 // Check connection
