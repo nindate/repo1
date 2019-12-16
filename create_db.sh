e1ae79b1f96e101fd2d3564ec84f210f2dcc5d68
nindate <nindate@gmail.com>
2019-12-16 10:09:27 +0530

    Create create_db.sh

---
+++ create_db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script to create database 'test2', user 'user1' and table structure required for contactsapp
+# You can provide appropriate password by replacing 'password' with your password after downloading the script
+mysql -u root << EOM
+create user 'user1'@'%' identified by 'password';
+create user 'user1'@'localhost' identified by 'password';
+create database test2;
+use test2;
+grant all privileges on test2.* to 'user1'@'%';
+grant all privileges on test2.* to 'user1'@'localhost';
+create table Persons (ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, FirstName CHAR(40) NOT NULL, LastName CHAR(40) NOT NULL, ContactNo BIGINT NOT NULL, Comments TEXT);
+EOM
