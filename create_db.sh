076da3775a57c8491d4853ca5df8b45d4f9fd34d
Ninad <nindate@gmail.com>
2023-01-13 14:01:43 +0530

    Update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -2,7 +2,7 @@
 
 # Script to create database 'test2', user 'user1' and table structure required for contactsapp
 # You can provide appropriate password by replacing 'password' with your password after downloading the script
-echo -e -n "\nCreating database\n"
+echo -e "\nCreating database\n"
 mysql -u dbuser -h dbhost -P dbport -pdbpass << EOM
 create user 'user1'@'%' identified by 'password';
 create user 'user1'@'localhost' identified by 'password';
@@ -12,4 +12,4 @@ grant all privileges on test2.* to 'user1'@'%';
 grant all privileges on test2.* to 'user1'@'localhost';
 create table Persons (ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, FirstName CHAR(40) NOT NULL, LastName CHAR(40) NOT NULL, ContactNo BIGINT NOT NULL, Comments TEXT);
 EOM
-echo -e -n "\nDatabase creation script run completed\n"
+echo -e "\nDatabase creation script run completed\n"
