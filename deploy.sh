4083c19efd39fc51632b16929d8966617b290bf6
Ninad <nindate@gmail.com>
2023-01-13 14:01:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
 clear
-echo -e -n "Will install packages, update scripts, web pages, create database and deploy application\n"
-echo -e -n "Please provide following information to proceed\n"
-echo -e -n "\nEnter database hostname : \c"
+echo -e "Will install packages, update scripts, web pages, create database and deploy application\n"
+echo -e "Please provide following information to proceed\n"
+echo -e "\nEnter database hostname : \c"
 read dbhost
-echo -e -n "\nEnter database port : \c"
-read dbport
-echo -e -n "\nEnter database user : \c"
+#echo -e "\nEnter database port : \c"
+#read dbport
+dbport=3306
+echo -e "\nEnter database user : \c"
 read dbuser
-echo -e -n "\nEnter password for user $dbuser : \c"
+echo -e "\nEnter password for user $dbuser : \c"
 read -s dbpass
+echo -e "\nEnter password again : \c"
+read -s dbpass2
 
-echo -e -n "\nInstalling Apache and PHP"
+if [[ ${dbpass} != ${dbpass2} ]]; then
+  echo -e "\nPasswords did not match. Exiting ...\n"
+  exit 1
+fi
+
+echo -e "\nInstalling Apache and PHP"
 sudo apt update -y && sudo apt install -y apache2 mariadb-client php php-mysql
 sudo systemctl restart apache2
 
-echo -e -n "\nWill update dbuser: $dbuser , dbhost: $dbhost , dbport: $dbport"
+echo -e "\nWill update dbuser: $dbuser , dbhost: $dbhost , dbport: $dbport"
 sudo sed -i -e "s/dbuser/$dbuser/g" -e "s/dbhost/$dbhost/g" -e "s/dbport/$dbport/g" -e "s/dbpass/$dbpass/g" create_db.sh site-contacts/*
 bash create_db.sh
 sudo mv site-contacts/* /var/www/html/
 
-echo -e -n "\nYou can now access your Contacts application by launching URL: http://your-server-name/index.php"
+echo -e "\nYou can now access your Contacts application by launching URL: http://your-server-name/index.php"
