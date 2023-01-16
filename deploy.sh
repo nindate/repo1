853c3a3d4329e6b1ff708d6c4b85af62acf107e3
Ninad <nindate@gmail.com>
2023-01-16 11:15:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,51 @@
 #!/bin/bash
 
 clear
-echo -e "Will install packages, update scripts, web pages, create database and deploy application\n"
-echo -e "Please provide following information to proceed\n"
-echo -e "\nEnter database hostname : \c"
-read dbhost
-#echo -e "\nEnter database port : \c"
-#read dbport
-dbport=3306
-echo -e "\nEnter database user : \c"
-read dbuser
-echo -e "\nEnter password for user $dbuser : \c"
-read -s dbpass
-echo -e "\nEnter password again : \c"
-read -s dbpass2
-
-if [[ ${dbpass} != ${dbpass2} ]]; then
-  echo -e "\nPasswords did not match. Exiting ...\n"
+echo -e "\nWill install packages, update scripts, web pages, create database and deploy application"
+echo -e "\nPlease provide following information to proceed"
+echo -e "\nAre you using locally installed MySQL database or PaaS instance (local / paas)? : \c"
+read dbtype
+if [[ ${dbtype} == "local" ]]; then
+  dbhost=localhost
+  dbuser=root
+  dbport=3306
+  mysql_conn_statement="sudo mysql -u root"
+elif [[ ${dbtype} == "paas" ]] ; then
+  echo -e "\nEnter database hostname : \c"
+  read dbhost
+  #echo -e "\nEnter database port : \c"
+  #read dbport
+  dbport=3306
+  echo -e "\nEnter database user : \c"
+  read dbuser
+  echo -e "\nEnter password for user $dbuser : \c"
+  read -s dbpass
+  echo -e "\nEnter password again : \c"
+  read -s dbpass2
+  if [[ ${dbpass} != ${dbpass2} ]]; then
+    echo -e "\nPasswords did not match. Exiting ...\n"
+    exit 1
+  fi
+  mysql_conn_statement="mysql -u ${dbuser} -h ${dbhost} -P 3306 -p${dbpass2}"
+else
+  echo -e "\nCorrect options are local and paas. Exiting ..."
   exit 1
 fi
+
 
 echo -e "\nInstalling Apache and PHP"
 sudo apt update -y && sudo apt install -y apache2 mariadb-client php php-mysql
 sudo systemctl restart apache2
 
-echo -e "\nUpdating dbuser: $dbuser , dbhost: $dbhost , dbport: $dbport"
-sudo sed -i -e "s/dbuser/$dbuser/g" -e "s/dbhost/$dbhost/g" -e "s/dbport/$dbport/g" -e "s/dbpass/$dbpass/g" create_db.sh site-contacts/*.php
+echo -e "\nUpdating scripts and web content"
+#echo -e "\nUpdating dbuser: $dbuser , dbhost: $dbhost , dbport: $dbport"
+#sudo sed -i -e "s/dbuser/$dbuser/g" -e "s/dbhost/$dbhost/g" -e "s/dbport/$dbport/g" -e "s/dbpass/$dbpass/g" create_db.sh site-contacts/*.php
+sudo sed -i -e "s/dbhost/$dbhost/g" -e "s/mysql_conn_statement/$mysql_conn_statement/g" create_db.sh site-contacts/*.php
 bash create_db.sh
+if [[ $? != 0 ]]; then
+  echo -e "\nThere was error while creating the database. Exiting ..."
+  exit 1
+fi
 echo -e "\nDeploying web content"
 sudo mv site-contacts/*.php /var/www/html/
 if [[ $? == 0 ]]; then
@@ -35,4 +54,4 @@ else
   echo -e "\nFailed to transfer web content to /var/www/html/"
 fi
 # Removing the default index.html file since we have index.php instead
-rm /var/www/html/index.html
+sudo rm -f /var/www/html/index.html
